d78e575dd0536ff42002f7bfbf9bf9745a67bd86
MrMan314 <44348373+MrMan314@users.noreply.github.com>
2024-03-22 00:12:27 -0400

    connect "front"-end and backend together

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ use http_body_util::{
 
 use hyper::{
 	body::{
+		Body,
 		Bytes,
 		Frame,
 	},
@@ -32,6 +33,10 @@ use hyper::{
 
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
+use serde_json::{
+	Value,
+	Value::Null,
+};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
@@ -47,7 +52,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 		let service = service_fn(move |req| {
 			let addr = addr.clone();
 			async move {
-				Ok::<Response<Full<Bytes>>, Infallible>(serve(req, addr).await)
+				match serve(req, addr).await {
+					Ok(resp) => Ok::<Response<Full<Bytes>>, Infallible>(resp),
+					Err(e) => {
+						println!("Error in serve: {:?}", e);
+						let mut resp = Response::new(Full::new(Bytes::from("Internal Server Error")));
+						*resp.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
+						return Ok(resp);
+					}
+				}
 			}
 		});
 
@@ -61,27 +74,65 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 	}
 }
 
-async fn serve(req: Request<hyper::body::Incoming>, addr: SocketAddr) -> Response<Full<Bytes>> {
+async fn serve(req: Request<hyper::body::Incoming>, addr: SocketAddr) -> Result<Response<Full<Bytes>>, Box<dyn std::error::Error>> {
 	match (req.method(), req.uri().path()) {
 		(&Method::POST, "/contact") => {
-			Response::new(Full::new(Bytes::from(format!("{}", addr))))
+			let max = req.body().size_hint().upper().unwrap_or(u64::MAX);
+			if max > 65536 {
+				let mut resp = Response::new(Full::new(Bytes::from("Request too long")));
+				*resp.status_mut() = StatusCode::PAYLOAD_TOO_LARGE;
+				return Ok(resp);
+			}
+
+			let body = &String::from_utf8(req.collect().await?.to_bytes().to_vec()).unwrap();
+
+			let data: Value = match serde_json::from_str(body) {
+				Ok(data) => data,
+				Err(e) => {
+					println!("Error in from_slice: {:?}", e);
+					let mut resp = Response::new(Full::new(Bytes::from("Bad Request")));
+					*resp.status_mut() = StatusCode::BAD_REQUEST;
+					return Ok(resp);
+				}
+			};
+
+			match post_comment(data, &format!("{addr}")).await {
+				Ok(resp) => return Ok(resp),
+				Err(e) => {
+					println!("Error in post_comment: {:?}", e);
+					let mut resp = Response::new(Full::new(Bytes::from("Bad Gateway")));
+					*resp.status_mut() = StatusCode::BAD_GATEWAY;
+					return Ok(resp);
+				},
+			};
 		},
 		_ => {
-			let mut resp = Response::new(Full::new(Bytes::from("test")));
+			let mut resp = Response::new(Full::new(Bytes::from("")));
 			resp.headers_mut().insert("Location", HeaderValue::from_static("https://gtohacks.github.io/website"));
 			*resp.status_mut() = StatusCode::FOUND;
-			resp
+			Ok(resp)
 		},
 	}
 }
 
-async fn post_comment(name: &str, title: &str, message: &str, email: &str, ipaddr: &str) -> Result<(), Box<dyn std::error::Error>> {
+async fn post_comment(data: Value, ipaddr: &str) -> Result<Response<Full<Bytes>>, Box<dyn std::error::Error>> {
 	let uri: &str = &env::var("WH_URI")?;
 
 	let now = SystemTime::now();
 	let now: DateTime<Utc> = now.into();
 	let now: &str = &now.to_rfc3339();
 	
+	if data["name"] == Null || data["title"] == Null || data["email"] == Null || data["message"] == Null {
+		let mut resp = Response::new(Full::new(Bytes::from("Bad Request")));
+		*resp.status_mut() = StatusCode::BAD_REQUEST;
+		return Ok(resp);
+	}
+
+	let name: &str = data["name"].as_str().unwrap();
+	let title: &str = data["title"].as_str().unwrap();
+	let email: &str = data["email"].as_str().unwrap();
+	let message: &str = data["message"].as_str().unwrap();
+
 	let params = &serde_json::json!({
 		"content": "new message!!! (totally)",
 		"username": name,
@@ -90,6 +141,10 @@ async fn post_comment(name: &str, title: &str, message: &str, email: &str, ipadd
 				"title": title,
 				"color": 0x2b2d31,
 				"fields": [
+					{
+						"name": "Name",
+						"value": name
+					},
 					{
 						"name": "Email",
 						"value": email,
@@ -107,16 +162,13 @@ async fn post_comment(name: &str, title: &str, message: &str, email: &str, ipadd
 
 	let client = reqwest::Client::new();
 
-	println!("{:#?}", params);
-
-	let resp = client.post(uri)
+	let wh_resp = client.post(uri)
 		.json(params)
 		.send()
-		.await?;/*
-		.text()
 		.await?;
-	*/
-	println!("{resp:#?}");
-	println!("{:#?}", resp.text().await?);
-	Ok(())
+	let status = wh_resp.status();
+	let body = wh_resp.bytes().await?;
+	let mut resp = Response::new(Full::new(Bytes::from(body)));
+	*resp.status_mut() = StatusCode::from_u16(status.into())?;
+	Ok(resp)
 }
